3e869fceaf6100aaa47cc9db652509d9b1ec3957
Lule-M <miha.luka1006@gmail.com>
2024-09-29 21:58:34 +0200

    Restructured the Tasks struct and fixed address class assignment

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 typedef struct
 {
-	int tasks[4][2];
+	int networks[4];
+	int computers[4];
 } Tasks;
 
 typedef struct 
@@ -28,42 +29,32 @@ Address GenerateRandomAddress(int min, int max)
 		address.octets[i] = generatedAddress;
 	}
 
-	address.class = rand_r(&seed) % (65 - 69 + 1) + 65;
+	if (address.octets[0] < 127)
+		address.class = 'A';
+	else if (address.octets[0] < 192)
+		address.class = 'B';
+	else
+		address.class = 'C';
+
 	return address;
 }
 
-Tasks GenerateAssignmentTasks()
+Tasks GenerateTasks()
 {
 	unsigned int seed = time(0);
 	
 	Tasks tasks;
-/*
- *					TASK
- *	a - # of networks	b - # of computers on each network
- *	
- *		 0	  1
- *
- *	0	{a1, b1}
- *	1	{a2, b2}
- *	2	{a3, b3}
- *	3	{a4, b4}	this task is always a point-to-point network
- *
- */
 
 	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 2; j++)
-		{
-			if (i == 3 && j == 1)
-			{
-				tasks.tasks[i][j] = 2;
-				continue;
-			}
+	{
+		tasks.networks[i] = rand_r(&seed) % (3 - 1 + 1) + 1;
 
-			if (j % 2 == 0)	// ensure we don't have a crazy amount of networks
-				tasks.tasks[i][j] = rand_r(&seed) % (3 - 1 + 1) + 1;
-			else
-				tasks.tasks[i][j] = rand_r(&seed) % (255 - 3 + 1) + 3;
-		}
+		if (i == 3)	// the last task is always a point-to-point network
+			tasks.computers[i] = 2;
+		else
+			tasks.computers[i] = rand_r(&seed) % (255 - 3 + 1) + 3;
+	}
+
 	return tasks;
 }
 
@@ -71,13 +62,12 @@ int main(int argc, char **argv)
 {
 	Address address = GenerateRandomAddress(10, 255);
 
-	Tasks tasks = GenerateAssignmentTasks();
+	Tasks tasks = GenerateTasks();
 	printf("Kompanija X poseduje opseg adresa klase %c, %d.%d.%d.%d. Treba napraviti šemu podmrežavanja tako da se obezbedi:\n", address.class, address.octets[0], address.octets[1], address.octets[2], address.octets[3]);
 	for (int i = 0; i < 4; i++)
-		if (tasks.tasks[i][0] == 1)
-			printf("- %d podmreža sa %d računara\n", tasks.tasks[i][0], tasks.tasks[i][1]);		
+		if (tasks.networks[i] == 1)
+		printf("- %d podmreža sa %d računara\n", tasks.networks[i], tasks.computers[i]);
 		else
-			printf("- %d podmreže sa %d računara\n", tasks.tasks[i][0], tasks.tasks[i][1]);		
-
+		printf("- %d podmreže sa %d računara\n", tasks.networks[i], tasks.computers[i]);
 	return 0;
 }
